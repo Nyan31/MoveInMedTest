1b3892c49f5f12f89fcf58a436b27869b14fb0c0
jeremyA31 <jeremyalsina.dev@gmail.com>
2021-03-12 16:30:08 +0100

    add layout of peoplePage / call data people & first work on searchBar

--- MoveInMedApp/src/app/people/people.page.ts
+++ MoveInMedApp/src/app/people/people.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { People } from '../models/people';
+import { PeopleService } from '../services/people.service';
 
 @Component({
   selector: 'app-people',
@@ -6,7 +8,103 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./people.page.scss'],
 })
 export class PeoplePage implements OnInit {
-  constructor() {}
+  arrayListOfAllPeople = [];
+  currentPreviousPagePeople: string;
+  currentNextPagePeople: string;
 
-  ngOnInit() {}
+  currentTextSearched: string;
+  arrayOfPeopleFromSearch = [];
+  tempResultOfPeopleSearch = [];
+
+  constructor(private peopleService: PeopleService) {}
+
+  ngOnInit() {
+    this.getAllDataPeople();
+  }
+
+  /**
+   * Call films service for get all data of movies
+   * Get details data for each api url array
+   */
+  getAllDataPeople() {
+    this.peopleService.getAllPeople().subscribe((data) => {
+      this.arrayListOfAllPeople = data.results;
+      this.currentNextPagePeople = data.next;
+      this.currentPreviousPagePeople = data.previous;
+      this.getHomeWorldOfCurrentListPeople();
+      this.getAllPeopleForArraySearch(data);
+    });
+  }
+
+  nextPagePeople() {
+    if (this.currentNextPagePeople) {
+      this.arrayListOfAllPeople = [];
+      this.peopleService
+        .getSpecificPageOfPeople(this.currentNextPagePeople)
+        .subscribe((data) => {
+          this.currentNextPagePeople = data.next;
+          this.currentPreviousPagePeople = data.previous;
+          this.arrayListOfAllPeople = data.results;
+          this.getHomeWorldOfCurrentListPeople();
+        });
+    }
+  }
+
+  previousPagePeople() {
+    if (this.currentPreviousPagePeople) {
+      this.arrayListOfAllPeople = [];
+      this.peopleService
+        .getSpecificPageOfPeople(this.currentPreviousPagePeople)
+        .subscribe((data) => {
+          this.currentNextPagePeople = data.next;
+          this.currentPreviousPagePeople = data.previous;
+          this.arrayListOfAllPeople = data.results;
+          this.getHomeWorldOfCurrentListPeople();
+        });
+    }
+  }
+
+  getHomeWorldOfCurrentListPeople() {
+    this.arrayListOfAllPeople.forEach((people) => {
+      this.peopleService
+        .getHomeworldOfSpecificPeople(people.homeworld)
+        .subscribe((data) => {
+          people.homeworld = data.name;
+        });
+    });
+  }
+
+  search(e) {
+    this.tempResultOfPeopleSearch = [];
+    this.arrayOfPeopleFromSearch.forEach(element => {
+      if(element.name.toLowerCase().includes(e.toString().toLowerCase())) {
+        const check = this.tempResultOfPeopleSearch.find(e => e.name === element.name);
+        if(!check) {
+          this.tempResultOfPeopleSearch.push(element);
+        }
+      }
+    });
+  }
+
+  getAllPeopleForArraySearch(data) {
+    let nbOfPages = (Math.ceil(data.count / 10) * 10) / 10;
+    this.arrayOfPeopleFromSearch = this.arrayListOfAllPeople;
+
+    for (let i = 2; i <= nbOfPages; i++) {
+      this.peopleService
+        .getSpecificPageOfPeople(
+          this.peopleService.apiUrl + 'people/?page=' + i
+        )
+        .subscribe((data) => {
+          data.results.forEach((element) => {
+            this.peopleService
+              .getHomeworldOfSpecificPeople(element.homeworld)
+              .subscribe((data) => {
+                element.homeworld = data.name;
+                this.arrayOfPeopleFromSearch.push(element);
+              });
+          });
+        });
+    }
+  }
 }
--- MoveInMedApp/src/app/services/people.service.ts
+++ MoveInMedApp/src/app/services/people.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { People } from '../models/people';
+import { Planets } from '../models/planets';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeopleService {
 
-  constructor() { }
+  apiUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllPeople(): Observable<any> {
+    return this.http.get<any>(this.apiUrl + 'people');
+  }
+
+  getSpecificPageOfPeople(url: string): Observable<any> {
+    return this.http.get<any>(url);
+  }
+
+  getHomeworldOfSpecificPeople(url: string): Observable<any> {
+    return this.http.get<Planets>(url);
+  }
+
 }
