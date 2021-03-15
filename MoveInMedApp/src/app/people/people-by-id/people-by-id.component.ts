c7213fbaa35c6af0907094720dc37c2bf2ca97d3
jeremyA31 <jeremyalsina.dev@gmail.com>
2021-03-15 12:03:52 +0100

    add layout page People by id and call data part2

--- MoveInMedApp/src/app/people/people-by-id/people-by-id.component.ts
+++ MoveInMedApp/src/app/people/people-by-id/people-by-id.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
+import { People } from 'src/app/models/people';
+import { PeopleService } from 'src/app/services/people.service';
 
 @Component({
   selector: 'app-people-by-id',
@@ -7,8 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeopleByIdComponent implements OnInit {
 
-  constructor() { }
+  @Input() specificPeopleReceived: People;
+  @Output() viewOfSpecificPeople = new EventEmitter<boolean>();
+
+  specificPeopleToDisplay: People;
+
+  constructor(private peopleService: PeopleService) { }
 
   ngOnInit() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.specificPeopleToDisplay = this.specificPeopleReceived;
+    this.getAllDetailsOfCurrentSpecificPeople();
+  }
+
+  back() {
+    this.viewOfSpecificPeople.emit(false);
+  }
+
+  getAllDetailsOfCurrentSpecificPeople() {
+    let temporaryArrayOfFilms = [];
+    let temporaryArrayOfSpecies = [];
+    let temporaryArrayOfStarships = [];
+    let temporaryArrayOfVehicles = [];
+
+    this.specificPeopleReceived.films.forEach(url => {
+      this.peopleService.getFilmOfSpecificPeople(url).subscribe((data) => {
+        temporaryArrayOfFilms.push(data);
+      })
+    });
+    this.specificPeopleReceived.species.forEach(url => {
+      this.peopleService.getFilmOfSpecificPeople(url).subscribe((data) => {
+        temporaryArrayOfSpecies.push(data);
+      })
+    });
+    this.specificPeopleReceived.starships.forEach(url => {
+      this.peopleService.getFilmOfSpecificPeople(url).subscribe((data) => {
+        temporaryArrayOfStarships.push(data);
+      })
+    });
+    this.specificPeopleReceived.vehicles.forEach(url => {
+      this.peopleService.getFilmOfSpecificPeople(url).subscribe((data) => {
+        temporaryArrayOfVehicles.push(data);
+      })
+    });
+
+    this.specificPeopleReceived.films = temporaryArrayOfFilms;
+    this.specificPeopleReceived.species = temporaryArrayOfSpecies;
+    this.specificPeopleReceived.starships = temporaryArrayOfStarships;
+    this.specificPeopleReceived.vehicles = temporaryArrayOfVehicles;
+  }
 
 }
--- MoveInMedApp/src/app/services/people.service.ts
+++ MoveInMedApp/src/app/services/people.service.ts
@@ -2,8 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
+import { Films } from '../models/films';
 import { People } from '../models/people';
 import { Planets } from '../models/planets';
+import { Species } from '../models/species';
+import { Starships } from '../models/starships';
+import { Vehicles } from '../models/vehicles';
 
 @Injectable({
   providedIn: 'root'
@@ -23,8 +27,22 @@ export class PeopleService {
     return this.http.get<any>(url);
   }
 
-  getHomeworldOfSpecificPeople(url: string): Observable<any> {
+  getHomeworldOfSpecificPeople(url: string): Observable<Planets> {
     return this.http.get<Planets>(url);
   }
+
+  getFilmOfSpecificPeople(url: any): Observable<any> {
+    return this.http.get<any>(url);
+  }
+  getSpecieOfSpecificPeople(url: any): Observable<any> {
+    return this.http.get<any>(url);
+  }
+  getStarshipOfSpecificPeople(url: any): Observable<any> {
+    return this.http.get<any>(url);
+  }
+  getVehicleOfSpecificPeople(url: any): Observable<any> {
+    return this.http.get<any>(url);
+  }
+
 
 }
